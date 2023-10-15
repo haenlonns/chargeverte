b01e07af3a49d0d1d2bb0322dc976dfa18c0ab3c
t-zhang21 <118033393+t-zhang21@users.noreply.github.com>
2023-10-15 04:31:13 -0400

    changed website

--- app.py
+++ app.py
@@ -8,17 +8,40 @@ from calculateMin import calculateMinCarbon
 
 data = getData("forecast")
 df = createDataFrame(data)
-minTime = calculateMinCarbon(df, 4)
 
-text = minTime
 inputText = 0
+start=0
+end=0
+numHours=0
+startTime=start
+
+minTime = calculateMinCarbon(df, numHours)
+
+def button_pressed(state):
+    state.numHours = int(state.numHours)
+    state.minTime = calculateMinCarbon(df, state.numHours)
+    print(state.numHours) 
+    print(state.minTime)
+    return state.minTime
+
 
 page = """
-# Getting started with Taipy GUI
 
-Ideal Charge Time: <|{text}|>
+<center>
+<|navbar|lov={[("page", "ChargeVerte")]}|>
+</center>
 
-<|{inputText}|input|>
+What interval do you want to charge during? (Enter times as 24-hour: ex. 6:30 is 1830)\n
+Start: <|{start}|input|> \n
+End: <|{end}|input|> \n
+How many hours do you need to charge for? \n
+Hours: <|{numHours}|input|> \n
+
+<|Calculate Optimal Time|button|on_action=button_pressed|>
+
+
+<|{minTime}|>
+
 """
 
-Gui(page).run()
+Gui(page).run(use_reloader=True, dark_mode=False)
